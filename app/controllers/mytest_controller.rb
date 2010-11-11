fe63aafbb02650164785c9f41f8aea2413a13cdf
John Mclain <glennlie@arribatel.com>
2010-11-12 03:26:01 +0800

    Initial commit

---
+++ app/controllers/mytest_controller.rb
@@ -0,0 +1,9 @@
+class MytestController < ApplicationController
+	def index
+		@title = "MyTest - Index"
+	end
+	
+	def home
+		@title = "MyTest - Home"
+	end
+end
---
+++ home/app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'd0e2c6cecf70892d8c712bc75308869e'
+end
